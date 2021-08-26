9ba00e3eb19f8b2dac96c90d07abc2d9db5a94d0
venu <venugopal.k1994@gmail.com>
2021-08-26 18:22:11 -0400

    prac

--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
 resource "aws_instance" "venu_instance_practice" {
     ami = "ami-02f84cf47c23f1769"
     instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.venu_securitygroupprac.name}"]
 
     tags = {
         Name = "venu_sharedLibrary_practice"
     }
 }
 
+resource "aws_security_group" "venu_securitygroupprac" {
+    name = "venu_2_securitygroup"
+    description = "venu security group"
+    ingress = [{
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "ingress"
+        from_port = 8080
+        ipv6_cidr_blocks = ["::/0"]
+        prefix_list_ids = []
+        protocol = "tcp"
+        security_groups = []
+        self = false
+        to_port = 8080
+    }]
+
+    tags = {
+        Name = "sharedlibrary_venu_sg_practice"
+    }
+}
