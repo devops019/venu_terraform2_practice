70c37a16d2790b54664146cac4b9fff4a154bea0
venu <venugopal.k1994@gmail.com>
2021-08-27 17:42:26 -0400

    f

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "devops019-terraformstatefile"
-        key = "devops019_practice"
+        key = "devops019key"
         region = "ca-central-1"
     }
 }
